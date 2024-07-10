c035853bf63c17409b910bfa62cef46380bb3356
oya ikumi <116304067+pokopoko7@users.noreply.github.com>
2024-07-10 09:33:33 +0900

    Update sample1-2.js

--- sample1-2.js
+++ sample1-2.js
@@ -83,7 +83,7 @@ function convertArray(data) {
   const dataString = data.split('\r\n');
   for (let i = 0; i < dataString.length; i++) {
     var data=dataString[i].split(',');
-    if (data[1]=='Ｒ３ー４０１'){ 
+    if (data[1]=='Ｒ３ーＢ１Ｆ_ＥＨ'){ 
       dataArray.push(data.slice(3,7).map(parseFloat));
     }
   }
